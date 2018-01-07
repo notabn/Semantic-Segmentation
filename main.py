8b1dc195d72e2debe86f012c413dd821bf904a48
notabn <natalia.balcu@gmail.com>
2018-01-07 20:34:44 +0100

    fixed loss msg

--- main.py
+++ main.py
@@ -147,9 +147,8 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
             _, loss = sess.run([train_op, cross_entropy_loss],
                                feed_dict={input_image: image, correct_label: label, keep_prob: 0.5,
                                           learning_rate: 0.0009})
-            print('loss', loss)
 
-        #print("Epoch %d of %d: Training loss: %.4f" % (epoch + 1, epochs, loss))
+        print("Epoch %d of %d: Training loss: %.4f" % (epoch + 1, epochs, loss))
 
 
 tests.test_train_nn(train_nn)
